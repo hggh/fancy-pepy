f68be7bcf7a88d0f374d901688957802520a63eb
Jonas Genannt <jonas@brachium-system.net>
2013-09-02 18:47:10 +0200

    rails4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 FancyPepy::Application.config.secret_token = '61a572fd59553f720bdde55177184a4d71d5b7916b8c073ac20ee81d3038f5a0e71f8e28ec7baa1819c89b61402d8dad6832f60bb09310941718891e6b3b5915'
+FancyPepy::Application.config.secret_key_base = 'fjdn8j61a572fd59553f720bdde55177184a4d71d5b7916b8c073ac20ee81d3038f5a0e71f8e28ec7baa1819c89b61402d8dad6832f60bb09310941718891e6b3b5915'
